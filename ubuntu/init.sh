b44237893e46c4c537f7df8912643accdd6943bb
liurui <liurui@lingyi365.com>
2017-02-13 17:54:54 +0800

    commit

--- ubuntu/init.sh
+++ ubuntu/init.sh
@@ -1,12 +1,12 @@
 apt_update(){
-    apt-get  update
-    apt-get -y upgrade
-    apt-get -y install openssh-server git  lrzsz   
+    sudo apt-get  update
+    sudo apt-get -y upgrade
+    sudo apt-get -y install openssh-server git  lrzsz wget
 }
 
 init_base_environment(){
     #下载包
-    git clone git@github.com:liu-rui/Shell.git
+    git clone https://github.com/liu-rui/Shell.git
     cd Shell/ubuntu
     #搜狗输入法
     wget -O sogou.deb  --tries 4  http://pinyin.sogou.com/linux/download.php?f=linux&bit=64 
@@ -14,9 +14,9 @@ init_base_environment(){
 }
 
 init_vim_environment(){
-    apt-get install ctags xclip vim-gnome astyle python-setuptools
+    sudo apt-get install ctags xclip vim-gnome astyle python-setuptools
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim  
-    cp bash/vim.sh ~/.vimrc
+    cp base/vim.sh ~/.vimrc
     vim +PluginInstall +qall
 }
 
@@ -28,15 +28,15 @@ init_vscode_environment(){
 }
 
 init_python_environment(){
-    apt-get -y install libc6-dev gcc make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev git libsqlite3-dev wget curl llvm
+    sudo  apt-get -y install libc6-dev gcc make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev git libsqlite3-dev wget curl llvm
     git clone https://github.com/yyuu/pyenv.git ~/.pyenv 
-    echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
-    echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile 
-    echo 'eval "$(pyenv init -)"' >> ~/.bash_profile
-    source ~/.bash_profile
+    echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+    echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+    echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+    source ~/.bashrc
     git clone https://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv 
-    echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bash_profile
-    source ~/.bash_profile
+    echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
+    source ~/.bashrc
     pyenv install  3.5.3 -v
     pyenv  virtualenv 3.5.3  dev3.5.3
     pyenv global  dev3.5.3
@@ -54,14 +54,10 @@ init_nodejs_environment(){
 
 
 main(){
-    if [[ "$(whoami)" != "root" ]]; then
-        echo "please run this script as root ." >&2
-        exit 1
-    fi
     echo -e "\033[31m 这个是ubuntu开发环境初始化脚本，请慎重运行！ press ctrl+C to cancel \033[0m"
     sleep 5
 
-    apt_update
+    #apt_update
     init_base_environment
     #init_vim_environment
     #init_python_environment

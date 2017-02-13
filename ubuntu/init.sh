a59d9a178489a13a7cd90254ec13eaaa6d4b2ccd
liurui <liurui@lingyi365.com>
2017-02-13 16:46:34 +0800

    commit

--- ubuntu/init.sh
+++ ubuntu/init.sh
@@ -1,14 +1,51 @@
 apt_update(){
-    sudo  apt-get  update
-    sudo  apt-get  upgrade
+    apt-get  update
+    apt-get -y upgrade
+    apt-get -y install openssh-server git  lrzsz   
+}
+
+init_base_environment(){
+    #下载包
+    git clone git@github.com:liu-rui/Shell.git
+    cd Shell/ubuntu
+    #搜狗输入法
+    wget -O sogou.deb  --tries 4  http://pinyin.sogou.com/linux/download.php?f=linux&bit=64 
+    dpkg  -i  sogou.deb
 }
 
 init_vim_environment(){
-    sudo apt-get install ctags xclip vim-gnome astyle python-setuptools
+    apt-get install ctags xclip vim-gnome astyle python-setuptools
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim  
+    cp bash/vim.sh ~/.vimrc
+    vim +PluginInstall +qall
+}
+
+init_vscode_environment(){    
+    wget -O code.deb  --tries 4  https://go.microsoft.com/fwlink/?LinkID=760868
+    dpkg  -i  code.deb
+    mkdir -p ~/.config/Code/User    
+    cp vscode/*  ~/.config/Code/User/
 }
 
 init_python_environment(){
+    apt-get -y install libc6-dev gcc make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev git libsqlite3-dev wget curl llvm
+    git clone https://github.com/yyuu/pyenv.git ~/.pyenv 
+    echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
+    echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile 
+    echo 'eval "$(pyenv init -)"' >> ~/.bash_profile
+    source ~/.bash_profile
+    git clone https://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv 
+    echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bash_profile
+    source ~/.bash_profile
+    pyenv install  3.5.3 -v
+    pyenv  virtualenv 3.5.3  dev3.5.3
+    pyenv global  dev3.5.3
 
+    python -m  pip  install --upgrade pip
+    python -m  pip  install flake8
+    python -m  pip  install pylint
+    python -m  pip  install autopep8
+    pip install jinja2
 }
 
 init_go_environment(){
@@ -16,7 +53,7 @@ init_go_environment(){
 }
 
 init_nodejs_environment(){
-
+    npm config set registry https://registry.npm.taobao.org
 }
 
 
@@ -29,6 +66,8 @@ main(){
     sleep 5
 
     apt_update
+    init_base_environment
+    init_vim_environment
     init_python_environment
     init_go_environment
     init_nodejs_environment

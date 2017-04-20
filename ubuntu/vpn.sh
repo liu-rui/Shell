0be7a46770b2093d654ed1cbb883ed395000ca5e
liurui <liurui@lingyi365.com>
2017-04-20 09:51:37 +0800

    update

--- ubuntu/vpn.sh
+++ ubuntu/vpn.sh
@@ -5,6 +5,7 @@ cd /data/software
 sudo apt-get install -y  openvpn
 wget http://172.18.112.106/openvpn.tar.gz
 sudo tar -zxf openvpn.tar.gz -C /etc/openvpn
+rm -f openvpn.tar.gz
 systemctl enable  openvpn
 systemctl restart openvpn
 ping  10.100.100.12

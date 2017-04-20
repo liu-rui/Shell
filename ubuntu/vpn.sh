b1aef684366e2a807d35c4d296aa62dfed035b57
liurui <liurui@lingyi365.com>
2017-04-20 09:57:47 +0800

    update

--- ubuntu/vpn.sh
+++ ubuntu/vpn.sh
@@ -2,7 +2,10 @@ user="$(whoami)"
 eval "sudo chown ${user}:${user} /data"
 mkdir /data/software    
 cd /data/software  
-sudo apt-get install -y -f openvpn
+sudo  apt-get  -f  install
+sudo apt-get install  -y openvpn
+sudo  apt-get  -f  install
+sudo apt-get install  -y openvpn
 wget http://172.18.112.106/openvpn.tar.gz
 sudo tar -zxf openvpn.tar.gz -C /etc/openvpn
 rm -f openvpn.tar.gz

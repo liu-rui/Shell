8ea64d116bb39e7a7a71a6beec7302c61aa2c71f
liurui <liurui@lingyi365.com>
2017-04-20 09:49:33 +0800

    update

---
+++ ubuntu/vpn.sh
@@ -0,0 +1,10 @@
+user="$(whoami)"
+eval "sudo chown ${user}:${user} /data"
+mkdir /data/software    
+cd /data/software  
+sudo apt-get install -y  openvpn
+wget http://172.18.112.106/openvpn.tar.gz
+sudo tar -zxf openvpn.tar.gz -C /etc/openvpn
+systemctl enable  openvpn
+systemctl restart openvpn
+ping  10.100.100.12
